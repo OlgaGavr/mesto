bea3f7bb1280ef226f08d36759a2453da68016d0
Olga Gavrilova <olgavrilova@gmail.com>
2020-12-25 13:22:59 +0300

    fix: исправить ошибки после 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,29 @@
 let openButton = document.querySelector('.button_action_edit');
 let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close');
+let closeButton = document.querySelector('.button_action_close');
+let formElement = document.querySelector('.form');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
 
 let togglePopup = () => {
     popup.classList.toggle('popup_opened');
 }
 
-let formElement = document.querySelector('.form');
 function openPopup() {
-  formElement.querySelector('.popup__field_text_name').value = document.querySelector('.profile__name').textContent;
-  formElement.querySelector('.popup__field_text_about').value = document.querySelector('.profile__about').textContent;
+  formElement.querySelector('.popup__field_text_name').value = profileName.textContent;
+  formElement.querySelector('.popup__field_text_about').value = profileAbout.textContent;
   togglePopup();
 }
-
-openButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', togglePopup);
 
 function handleFormSubmit (evt) {
   evt.preventDefault(); 
   let nameInput = formElement.querySelector('.popup__field_text_name');
   let jobInput =  formElement.querySelector('.popup__field_text_about');
-  document.querySelector('.profile__name').textContent =  nameInput.value;
-  document.querySelector('.profile__about').textContent =   jobInput.value;
+  profileName.textContent =  nameInput.value;
+  profileAbout.textContent =   jobInput.value;
   togglePopup();
 }
-
+openButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', togglePopup);
 formElement.addEventListener('submit', handleFormSubmit); 
 
