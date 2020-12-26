7ba5ec0ab949c8655219e7f811771969bbf26f11
Olga Gavrilova <olgavrilova@gmail.com>
2020-12-26 12:04:20 +0300

    fix: исправить ошибки после 2 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,29 @@
 let openButton = document.querySelector('.button_action_edit');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.button_action_close');
-let formElement = document.querySelector('.form');
+let formElement = popup.querySelector('.form');
+
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
+let popupName = formElement.querySelector('.popup__field_text_name');
+let popupAbout = formElement.querySelector('.popup__field_text_about');
 
 let togglePopup = () => {
     popup.classList.toggle('popup_opened');
-}
+}  
+
 
 function openPopup() {
-  formElement.querySelector('.popup__field_text_name').value = profileName.textContent;
-  formElement.querySelector('.popup__field_text_about').value = profileAbout.textContent;
+  popupName.value = profileName.textContent;
+  popupAbout.value = profileAbout.textContent;
   togglePopup();
+  
 }
 
 function handleFormSubmit (evt) {
   evt.preventDefault(); 
-  let nameInput = formElement.querySelector('.popup__field_text_name');
-  let jobInput =  formElement.querySelector('.popup__field_text_about');
-  profileName.textContent =  nameInput.value;
-  profileAbout.textContent =   jobInput.value;
+  profileName.textContent = popupName.value;
+  profileAbout.textContent =  popupAbout.value;
   togglePopup();
 }
 openButton.addEventListener('click', openPopup);
