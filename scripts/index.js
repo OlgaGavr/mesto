13e709157e6b8f6ad176b322aa2d98159942fa7c
Olga Gavrilova <olgavrilova@gmail.com>
2021-01-22 12:55:15 +0300

    fix: Исправить можно лучше

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,3 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-]; 
 const editButton = document.querySelector('.button_action_edit');
 const addButton = document.querySelector('.button_action_add');
 const popupEdit = document.querySelector('.popup_type_edit');
@@ -46,7 +20,7 @@ const popupLink = formAdd.querySelector('.popup__field_text_about');
 const itemTemplate = document.querySelector('.item-template').content;
 const cards = document.querySelector('.cards');
 
-function render(){
+function render(masCard){
   initialCards.forEach((card) => {
       renderCard(createCard(card), cards)
   });
@@ -133,5 +107,5 @@ formEdit.addEventListener('submit', handleSubmitEdit);
 formAdd.addEventListener('submit', handleSubmitAdd);
 
 
-render();
+render(initialCards);
 
